76052b132a87a1d531cf68f4f44c91e75359004e
ntp1504 <phatnguyentien1504@gmail.com>
2022-04-23 18:52:15 +0700

    Update Student

--- AttendanceManagementSystem/Student.cs
+++ AttendanceManagementSystem/Student.cs
@@ -8,13 +8,26 @@ using Google.Cloud.Firestore;
 namespace AttendanceManagementSystem
 {
     [FirestoreData]
-    internal class Student
+    public class Student
     {
+
         [FirestoreProperty]
         public int StudentID { get; set; }
         [FirestoreProperty]
         public string Name { get; set; }
         [FirestoreProperty]
         public string Email { get; set; }
+        [FirestoreProperty]
+        public string CardId { get; set; }
+
+        public Student() { }
+
+        public Student(int studentID, string name, string email, string cardId)
+        {
+            StudentID = studentID;
+            Name = name;
+            Email = email;
+            CardId = cardId;
+        }
     }
 }
