d5d9e195d982a7c34998f38ce01d5b82364d33fa
pdattt <83204029+pdattt@users.noreply.github.com>
2022-04-24 19:25:56 +0700

    Add Model Event

--- AttendanceManagementSystem/DAO/AttendeeDAO.cs
+++ AttendanceManagementSystem/DAO/AttendeeDAO.cs
@@ -1,5 +1,4 @@
-﻿using AttendanceManagementSystem;
-using Google.Cloud.Firestore;
+﻿using Google.Cloud.Firestore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +10,6 @@ namespace DoAnLapTrinhA.DAO
     public class AttendeeDAO
     {
         FirestoreDb db;
-        List<Attendee> listAttendee;
 
         public AttendeeDAO()
         {
@@ -19,21 +17,6 @@ namespace DoAnLapTrinhA.DAO
             Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
 
             db = FirestoreDb.Create("attendancerfid-a6f84");
-        }
-
-        public async ValueTask<List<Attendee>> GetAll()
-        {
-            listAttendee = new List<Attendee>();
-
-            Query qref = db.Collection("Attendee");
-            QuerySnapshot snap = await qref.GetSnapshotAsync();
-
-            foreach (DocumentSnapshot docsnap in snap)
-            {
-                Attendee attendee = docsnap.ConvertTo<Attendee>();
-                listAttendee.Add(attendee);
-            }
-            return listAttendee;
         }
     }
 }
---
+++ AttendanceManagementSystem/Model/Event.cs
@@ -0,0 +1,31 @@
+﻿using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttendanceManagementSystem.Model
+{
+    [FirestoreData]
+    public class Event
+    {
+        [FirestoreProperty]
+        public int EventID { get; set; }
+
+        [FirestoreProperty]
+        public string EventName { get; set; }
+
+        [FirestoreProperty]
+        public string EventDate { get; set; }
+
+        [FirestoreProperty]
+        public string Location { get; set; }
+
+        [FirestoreProperty]
+        public string EventStartTime { get; set; }
+
+        [FirestoreProperty]
+        public string EventEndTime { get; set; }
+    }
+}
